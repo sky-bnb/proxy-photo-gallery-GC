cde437753a56bb80ca1474a60dd35782eae2ffec
George Chen <georgechen@Georges-MacBook-Air-3.local>
2019-06-01 22:06:44 -0700

    added style to proxy

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const proxy = require('http-proxy-middleware')
 
 app.use(express.static('public'))
+
+app.use('/MoreHomes',
+proxy({
+    target: 'http://3.14.81.50/MoreHomes',
+    pathRewrite: (path, req) => {
+        return path.split('/').slice(2).join('/');
+    }
+})
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
